b563bdebfe05658fd9e69de370b543a2875fbcf8
Adriel <49051133+izuca@users.noreply.github.com>
2019-04-03 13:07:37 -0300

    Create 1016.cpp

---
+++ Iniciante/1016.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+
+using namespace std;
+
+int main ()
+{
+	int distancia;
+	
+	cin >> distancia;
+	cout << distancia * 2 << " minutos" << endl;
+
+
+	return 0;
+}
