c1e0044454feab2e6340fd3a2a5d84eda69c84cc
Adriel <49051133+izuca@users.noreply.github.com>
2019-04-03 13:07:17 -0300

    Create 1015.cpp

---
+++ Iniciante/1015.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+
+using namespace std;
+
+int main ()
+{
+	float x1,y1,x2,y2;
+	
+	cin >> x1 >> y1 >> x2 >> y2;
+	cout << fixed << setprecision(4);
+	cout << pow(pow((x2-x1),2) + pow(y2-y1,2),0.5) << endl;
+	
+	
+	
+	
+	return 0;
+}
