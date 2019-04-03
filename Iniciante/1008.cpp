c31f6c67f59497bc3b2246676c0104722004ea53
Adriel <49051133+izuca@users.noreply.github.com>
2019-04-03 13:03:26 -0300

    Create 1008.cpp

---
+++ Iniciante/1008.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    int n1,qtdh;
+    float vH;
+
+    cout<<fixed<<setprecision(2);
+    cin>>n1>>qtdh>>vH;
+    cout<<"NUMBER = "<<n1<< "\nSALARY = U$ "<<qtdh*vH<<endl;
+
+    return 0;
+
+
+
+
+}
