9eb419cbdcb681a1c7c65a2944af90038612a155
Adriel <49051133+izuca@users.noreply.github.com>
2019-04-03 13:06:56 -0300

    Create 1014.cpp

---
+++ Iniciante/1014.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+
+using namespace std;
+
+int main ()
+{
+	int distanciap;
+	float combustivelg;
+	
+	cout << fixed << setprecision(3);
+	cin >> distanciap >> combustivelg;
+	cout << distanciap / combustivelg << " km/l" << endl;
+	
+	
+	
+	
+	
+	
+	return 0;
+}
